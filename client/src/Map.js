c4e8ab9ea2407e9dcd6f98626bcfa4dfa07fae71
Jorge Muñoz <jorge.mg@cetys.edu.mx>
2021-05-20 15:36:16 -0700

    Console logging clicking location
    Modify db to store lat and lng to make marker show in the event view, modify db.

--- client/src/Map.js
+++ client/src/Map.js
@@ -7,7 +7,7 @@ function Map() {
         height: '30rem'
     };
     const center = {
-        lat: 23.281909,
+        lat: 23.575287,
         lng: -102.499577
     };
     const { isLoaded } = useJsApiLoader({
@@ -17,6 +17,7 @@ function Map() {
     })
 
     const [map, setMap] = React.useState(null)
+    const [marker, setMarker] = React.useState({})
 
     const onLoad = React.useCallback(function callback(map) {
         const bounds = new window.google.maps.LatLngBounds();
@@ -34,9 +35,20 @@ function Map() {
         <GoogleMap
             mapContainerStyle={containerStyle}
             center={center}
-            zoom={10}
+            zoom={5}
             onLoad={onLoad}
             onUnmount={onUnmount}
+            onClick={(e) => {
+                console.log(e.latLng.lat())
+                console.log(e.latLng.lng())
+                setMarker({
+                    lat: e.latLng.lat(),
+                    lng: e.latLng.lng()
+                })
+
+                console.log(marker.lat)
+                console.log(marker.lng)
+            }}
         >
             { /* Child components, such as markers, info windows, etc. */}
             <></>
